4b924af0d371ef794c0f1c774e005100c63457f9
dimitrits25 <103533278+dimitrits25@users.noreply.github.com>
2022-04-20 17:06:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -104,12 +104,11 @@ let woorden2 = document.getElementById("woorden2");
        woorden.innerHTML = "";
        localStorage.setItem("laatste woord", "");
        localStorage.setItem("alleWoorden", "");
-        if(localStorage.getItem("allewoorden") != null | undefined){
             woorden2.innerHTML = localStorage.getItem("alleWoorden");
-       }
     })
-
-    woorden2.innerHTML += localStorage.getItem("alleWoorden");
+        if(localStorage.getItem("allewoorden") != null | undefined){
+        woorden2.innerHTML += localStorage.getItem("alleWoorden");
+    }
     woorden.innerHTML = localStorage.getItem("laatste woord");
 
 
